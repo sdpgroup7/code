fd9151c19520283dec4fd30b1763fa8226e66948
David Fraser <david@dephraser.net>
2012-01-25 15:42:43 +0000

    Added the enum op codes

--- Control/src/uk/ac/ed/inf/sdp2012/group7/shared/ConstantsReuse.java
+++ Control/src/uk/ac/ed/inf/sdp2012/group7/shared/ConstantsReuse.java
@@ -6,4 +6,20 @@ public class ConstantsReuse {
 
 	public static String ROBOT_NAME = "Hacktar";
 	public static String ROBOT_MAC = "00:16:53:08:9F:AF";
+
+    public enum OpCodes {
+        DO_NOTHING,
+        FORWARDS,
+        BACKWARDS,
+        BACKWARDS_SLIGHTLY,
+        STOP,
+        CHANGE_SPEED,
+        KICK,
+        ROTATE,
+        ARC,
+        STEER_WITH_RATIO,
+        BEEP,
+        CELEBRATE
+    }
+
 }
