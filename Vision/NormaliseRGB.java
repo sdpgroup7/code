e9c4a1bb5331e1a68d9e5374231398f4818ef53d
Dale Myers <s0942590@corran.inf.ed.ac.uk>
2012-01-25 19:58:08 +0000

    Fixed the issue with NormaliseRBG - it now works correctly
    Basically integers were being divided by numbers bigger than themselves, or by themselves resulting in a fraction or 1. Which meant you either got 0 or 255 out due to it being integers.

--- Vision/NormaliseRGB.java
+++ Vision/NormaliseRGB.java
@@ -7,14 +7,15 @@ public class NormaliseRGB{
         for (int i = 0; i < 640; i++){
             for (int j = 0; j < 480; j++){
                 Color rgb = new Color(image.getRGB(i,j));
-                int a = rgb.getAlpha();
-                int r = rgb.getRed();
-                int g = rgb.getGreen();
-                int b = rgb.getBlue();
+                float a = rgb.getAlpha();
+                float r = rgb.getRed();
+                float g = rgb.getGreen();
+                float b = rgb.getBlue();
                 r = (r/(r+g+b))*255;
                 g = (g/(r+g+b))*255;
                 b = (b/(r+g+b))*255;
-                image.setRGB(i,j,r*g*b);
+				int argb = ((int)a << 24) + (((int)r & 0xFF) << 16) + (((int)g & 0xFF) << 8) + ((int)b & 0xFF);
+                image.setRGB(i,j,argb);
             }
         }
         return image;
