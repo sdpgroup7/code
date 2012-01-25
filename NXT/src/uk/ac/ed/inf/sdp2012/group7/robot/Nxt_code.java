1ee7d377ab85c128895a6126b460ad9e7ae05f48
Tomas Tauber <tomas.tauber@gmail.com>
2012-01-25 16:09:02 +0000

    A bit of clean up in the NXT code.

--- NXT/src/uk/ac/ed/inf/sdp2012/group7/robot/Nxt_code.java
+++ NXT/src/uk/ac/ed/inf/sdp2012/group7/robot/Nxt_code.java
@@ -1,5 +1,4 @@
 package uk.ac.ed.inf.sdp2012.group7.robot;
-import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -10,25 +9,23 @@ import lejos.nxt.Sound;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.comm.Bluetooth;
 import lejos.nxt.comm.NXTConnection;
-import lejos.robotics.navigation.Move;
-import lejos.robotics.navigation.MoveListener;
-import lejos.robotics.navigation.MoveProvider;
 import lejos.robotics.navigation.DifferentialPilot;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 /**
  * Code that runs on the NXT brick
  */
-public class Nxt_code implements Runnable, MoveListener {
+public class Nxt_code implements Runnable {
 
 	// class variables
-	private static boolean spinnersOn = false;
 	private static InputStream is;
 	private static OutputStream os;
 	private static DifferentialPilot pilot;
-	private static boolean ignoringInput = false;
 	private static volatile boolean blocking = false;
 	private static volatile boolean kicking = false;
-
+	private static Logger logger = Logger.getRootLogger();
 	// constants for the pilot class 
 	private static final float TRACK_WIDTH = (float) 14.6;
 	private static final float WHEEL_DIAMETER = (float) 81.6;
@@ -43,11 +40,7 @@ public class Nxt_code implements Runnable, MoveListener {
 	private final static int KICK = 0X06;
 	private final static int ROTATE = 0X07;
 	private final static int ARC = 0X08;
-	private final static int ADJUST_WHEEL_SPEEDS = 0X09;
-	private final static int LEFT_MOTOR_FORWARDS = 0X0A;
-	private final static int RIGHT_MOTOR_FORWARDS = 0X0B;
-	private final static int LEFT_MOTOR_BACKWARDS = 0X0C;
-	private final static int RIGHT_MOTOR_BACKWARDS = 0X0D;
+
 	private final static int STEER_WITH_RATIO = 0X0E;
 	private final static int BEEP = 0X0F;
 	private final static int CELEBRATE = 0X10;
@@ -56,7 +49,7 @@ public class Nxt_code implements Runnable, MoveListener {
 	public static void main(String[] args) throws Exception {
 
 		DifferentialPilot pilot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, Motor.B,Motor.C, false);
-		
+		logger.setLevel((Level) Level.WARN);
 		// start the sensor thread
 		new Thread(new Nxt_code(pilot)).start();
 
@@ -67,7 +60,6 @@ public class Nxt_code implements Runnable, MoveListener {
 
 		while (true) {
 			try {
-
 				// wait for a connection and open streams
 				pilot.stop();
 				LCD.clear();
@@ -101,178 +93,101 @@ public class Nxt_code implements Runnable, MoveListener {
 					
 					switch (opcode) {
 
-					case FORWARDS:
-						if (pilot.isMoving()) {
-							break;
-						} else {
-							pilot.forward();
-							break;
-						}
-
-					case BACKWARDS:
-						if (pilot.isMoving()) {
-							break;
-						} else {
-							pilot.backward();
-							break;
-						}
-
-					case BACKWARDS_SLIGHTLY: // back up a little
-						pilot.travel(-10);
-						break;
-
-					case STOP:
-						pilot.stop();
-						break;
-
-					case CHANGE_SPEED:
-						pilot.setTravelSpeed((n >> 8));
-						break;
-
-					case KICK:
-						Thread Kick_thread = new Thread() {
-							public void run() {
-								Motor.A.setSpeed(900);
-								
-								Motor.A.rotate(-60, true);
-								try {
-									Thread.sleep(300);
-								} catch (InterruptedException e) {
-								}
-								Motor.A.setSpeed(60);
-								Motor.A.rotate(60, true);
-								/*try {
-									Thread.sleep(300);
-								} catch (InterruptedException e) {
-								}
-								//Motor.A.rotate(-1, true);
-								try {
-									Thread.sleep(300);
-								} catch (InterruptedException e) {
-								}*/
-								//Motor.A.stop();
-								kicking = false;
+						case FORWARDS:
+							if (pilot.isMoving()) {
+								break;
+							} else {
+								pilot.forward();
+								break;
 							}
-						};
-						if (!kicking) {
-							kicking = true;
-							Kick_thread.start();
-						}
-						break;
-
-					case ROTATE:
-
-						int rotateBy = n >> 8;
-
-						// if n > 360 change to negative (turn left)
-						if (rotateBy > 360) {
-							rotateBy = -(rotateBy - 360);
-						}
-						// by setting immediate return to false, this method
-						// blocks
-						// until the rotation is complete
-						blocking = true;
-						pilot.rotate(rotateBy, false);
-						blocking = false;
-						break;
-
-					case ARC:
-
-						int arcRadius = n >> 8;
-
-						// if n > 1000 change to negative (turn left)
-						if (arcRadius > 1000) {
-							arcRadius = (arcRadius - 1000);
-						}
-						pilot.arcForward(arcRadius);
-						break;
-
-					case ADJUST_WHEEL_SPEEDS: // set wheel speeds and direction
-						// independently
-
-						boolean leftShouldBeGoingBackwards = false;
-						boolean rightShouldBeGoingBackwards = false;
-
-						// get the left wheel speed sign bit
-						if (((n << 12) >> 31) == -1) {
-							leftShouldBeGoingBackwards = true;
-						}
-
-						// get the right wheel speed sign bit
-						if (n >> 31 == -1) {
-							rightShouldBeGoingBackwards = true;
-						}
-
-						// set the left wheel speed
-						Motor.B.setSpeed((n << 13) >> 21);
-
-						// get the right wheel speed
-						Motor.C.setSpeed((n << 1) >> 21);
-
-						// set the left wheel direction
-						if (leftShouldBeGoingBackwards
-								&& pilot.isMoving()) {
-							Motor.B.backward();
-						} else if (!leftShouldBeGoingBackwards
-								&& pilot.isMoving()) {
-							Motor.B.forward();
-						}
-
-						// set the right wheel direction
-						if (rightShouldBeGoingBackwards
-								&& pilot.isMoving()) {
-							Motor.B.backward();
-						} else if (!rightShouldBeGoingBackwards
-								&& pilot.isMoving()) {
-							Motor.B.forward();
-						}
-
-						break;
-
-					case LEFT_MOTOR_FORWARDS: // sweep right
-						if (pilot.isMoving()) {
-							Motor.B.forward();
-						}
-						break;
-
-					case RIGHT_MOTOR_FORWARDS: // sweep left
-						if (pilot.isMoving()) {
-							Motor.C.forward();
-						}
-						break;
-
-					case LEFT_MOTOR_BACKWARDS:
-						if (pilot.isMoving()) {
-							Motor.B.backward();
-						}
-						break;
-
-					case RIGHT_MOTOR_BACKWARDS:
-						if (pilot.isMoving()) {
-							Motor.C.backward();
-						}
-						break;
-
-					case STEER_WITH_RATIO:
-						pilot.steer(n >> 8);
-						break;
-
-					case BEEP:
-						Sound.beep();
-						break;
-
-					case CELEBRATE: // play a sound file
-						try {
-							File f = new File("Get_Item.wav");
-							Sound.playSample(f, 0);
-						} catch (Exception e) {
-						}
-						break;
-
-					case QUIT: // close connection
-						Sound.twoBeeps();
-						break;
-
+	
+						case BACKWARDS:
+							if (pilot.isMoving()) {
+								break;
+							} else {
+								pilot.backward();
+								break;
+							}
+	
+						case BACKWARDS_SLIGHTLY: // back up a little
+							pilot.travel(-10);
+							break;
+	
+						case STOP:
+							pilot.stop();
+							break;
+	
+						case CHANGE_SPEED:
+							pilot.setTravelSpeed((n >> 8));
+							break;
+	
+						case KICK:
+							Thread Kick_thread = new Thread() {
+								public void run() {
+									Motor.A.setSpeed(900);
+									
+									Motor.A.rotate(-60, true);
+									try {
+										Thread.sleep(300);
+									} catch (InterruptedException e) {
+										logger.debug("Kick: interrupted during waiting", e);
+									}
+									Motor.A.setSpeed(60);
+									Motor.A.rotate(60, true);
+									
+									kicking = false;
+								}
+							};
+							if (!kicking) {
+								kicking = true;
+								Kick_thread.start();
+							}
+							break;
+	
+						case ROTATE:
+	
+							int rotateBy = n >> 8;
+	
+							// if n > 360 change to negative (turn left)
+							if (rotateBy > 360) {
+								rotateBy = -(rotateBy - 360);
+							}
+							// by setting immediate return to false, this method
+							// blocks
+							// until the rotation is complete
+							blocking = true;
+							pilot.rotate(rotateBy, false);
+							blocking = false;
+							break;
+	
+						case ARC:
+	
+							int arcRadius = n >> 8;
+	
+							// if n > 1000 change to negative (turn left)
+							if (arcRadius > 1000) {
+								arcRadius = (arcRadius - 1000);
+							}
+							pilot.arcForward(arcRadius);
+							break;
+	
+	
+						case STEER_WITH_RATIO:
+							pilot.steer(n >> 8);
+							break;
+	
+						case BEEP:
+							Sound.beep();
+							break;
+	
+						case CELEBRATE: // play a sound file
+							Sound.beepSequenceUp();
+							
+							break;
+	
+						case QUIT: // close connection
+							Sound.twoBeeps();
+							break;
 					}
 
 					// respond to say command was acted on
@@ -289,6 +204,7 @@ public class Nxt_code implements Runnable, MoveListener {
 			} catch (Exception e) {
 				LCD.clear();
 				LCD.drawString("EXCEPTION!", 0, 2);
+				logger.error("Something went wrong in the main thread.", e);
 			}
 		}
 
@@ -311,7 +227,7 @@ public class Nxt_code implements Runnable, MoveListener {
 	 * main(), so that pilot is accessible within the sensor thread.
 	 */
 	public Nxt_code(DifferentialPilot pilot) {
-		this.pilot = pilot;
+		Nxt_code.pilot = pilot;
 	}
 
 	/**
@@ -322,12 +238,18 @@ public class Nxt_code implements Runnable, MoveListener {
 
 		boolean reacting = false;
 		float tempCurSpeed;
-		TouchSensor touchA = new TouchSensor(SensorPort.S1);
-		TouchSensor touchB = new TouchSensor(SensorPort.S2);
+		
+		TouchSensor touchKick = new TouchSensor(SensorPort.S1);
+		
+		TouchSensor touchA = new TouchSensor(SensorPort.S2);
+		TouchSensor touchB = new TouchSensor(SensorPort.S3);
 
 		while (true) {
 			try {
-
+				if (touchKick.isPressed()) {
+					Motor.A.setSpeed(10);
+					Motor.A.rotate(-1,true);
+				}
 				if (!reacting && (touchA.isPressed() || touchB.isPressed())) {
 
 					// flag sensor hit as being dealt with and save the speed
@@ -356,21 +278,10 @@ public class Nxt_code implements Runnable, MoveListener {
 				Thread.sleep(50);
 
 			} catch (Exception ex) {
+				logger.error("Something went wrong in the sensor thread.", ex);
 			}
 
 		}
-	}
-
-	@Override
-	public void moveStarted(Move event, MoveProvider mp) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void moveStopped(Move event, MoveProvider mp) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
