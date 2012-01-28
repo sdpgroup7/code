030328167daf20f39470918fe76ca58335a95ceb
James Hulme <s0901522@sms.ed.ac.uk>
2012-01-28 17:57:36 +0000

    removed hsb from object detection, just uses RGB

--- Vision/VisionFeed.java
+++ Vision/VisionFeed.java
@@ -51,16 +51,6 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
     private ControlGUI thresholdGUI;
     private static boolean buffersSet = false;
     private static Point mouseCo = new Point(0,0);
-    private int[] hsbLowBall = new int[3];
-    private int[] hsbHighBall = new int[3];
-    private int[] hsbLowBlue = new int[3];
-    private int[] hsbHighBlue = new int[3];
-    private int[] hsbLowYellow = new int[3];
-    private int[] hsbHighYellow = new int[3];
-    private int[] hsbLowGreen = new int[3];
-    private int[] hsbHighGreen = new int[3];
-    private int[] hsbLowGrey = new int[3];
-    private int[] hsbHighGrey = new int[3];
 
     /**
      * Default constructor.
@@ -148,25 +138,15 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
     //Set the sliders on the GUI, the messages are used to tell the user what to click
     public void getColors(){
         thresholdGUI.setBallValues(getClickColor("Click the ball"));
-        hsbLowBall = hsbValues(thresholdsState.getBall_r_low(),thresholdsState.getBall_g_low(),thresholdsState.getBall_b_low(),-25);
-        hsbHighBall = hsbValues(thresholdsState.getBall_r_high(),thresholdsState.getBall_g_high(),thresholdsState.getBall_b_high(),25);
         
         thresholdGUI.setYellowValues(getClickColor("Click the yellow robot"));
-        hsbLowYellow = hsbValues(thresholdsState.getYellow_r_low(),thresholdsState.getYellow_g_low(),thresholdsState.getYellow_b_low(),-25);
-        hsbHighYellow = hsbValues(thresholdsState.getYellow_r_high(),thresholdsState.getYellow_g_high(),thresholdsState.getYellow_b_high(),25);
         
         thresholdGUI.setBlueValues(getClickColor("Click the blue robot"));
-        hsbLowBlue = hsbValues(thresholdsState.getBlue_r_low(),thresholdsState.getBlue_g_low(),thresholdsState.getBlue_b_low(),-25);
-        hsbHighBlue = hsbValues(thresholdsState.getBlue_r_high(),thresholdsState.getBlue_g_high(),thresholdsState.getBlue_b_high(),25);
         
         thresholdGUI.setGreenValues(getClickColor("Click a green plate"));
-        hsbLowGreen = hsbValues(thresholdsState.getGreen_r_low(),thresholdsState.getGreen_g_low(),thresholdsState.getGreen_b_low(),-25);
-        hsbHighGreen = hsbValues(thresholdsState.getGreen_r_high(),thresholdsState.getGreen_g_high(),thresholdsState.getGreen_b_high(),25);
 
         
         thresholdGUI.setGreyValues(getClickColor("Click a grey circle"));
-        hsbLowGrey = hsbValues(thresholdsState.getGrey_r_low(),thresholdsState.getGrey_g_low(),thresholdsState.getGrey_b_low(),-25);
-        hsbHighGrey = hsbValues(thresholdsState.getGrey_r_high(),thresholdsState.getGrey_g_high(),thresholdsState.getGrey_b_high(),25);
         
     }
     
@@ -383,21 +363,19 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
                 //System.err.println("column,row = " + column + "," + row);
                 /* The RGB colours and hsv values for the current pixel. */
                 Color c = new Color(image.getRGB(column, row));
-                float hsbvals[] = new float[3];
-                Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
 
                 /* Debug graphics for the grey circles and green plates.
                  * TODO: Move these into the actual detection. */
-                if (thresholdsState.isGrey_debug() && isGrey(c, hsbvals)) {
+                if (thresholdsState.isGrey_debug() && isGrey(c)) {
                     image.setRGB(column, row, 0xFFFF0099);
                 }
 
-                if (thresholdsState.isGreen_debug() && isGreen(c, hsbvals)) {
+                if (thresholdsState.isGreen_debug() && isGreen(c)) {
                     image.setRGB(column, row, 0xFFFF0099);
                 }
 
                 /* Is this pixel part of the Blue T? */
-                if (isBlue(c, hsbvals) ){
+                if (isBlue(c) ){
 
                     blueX += column;
                     blueY += row;
@@ -415,7 +393,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
                 }
 
                 /* Is this pixel part of the Yellow T? */
-                if (isYellow(c, hsbvals)) {
+                if (isYellow(c)) {
 
                     yellowX += column;
                     yellowY += row;
@@ -432,7 +410,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
                 }
 
                 /* Is this pixel part of the Ball? */
-                if (isBall(c, hsbvals)) {
+                if (isBall(c)) {
 
                     ballX += column;
                     ballY += row;
@@ -571,22 +549,9 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
      *                      thresholds (and thus the pixel is part of the blue T),
      *                      false otherwise.
      */
-    
-    private int[] hsbValues(int r, int g, int b, int threshold){
-        float[] hsb = new float[3];
-        int[] hsbInt = new int[3];
-        Color.RGBtoHSB(r,g,b,hsb);
-        for(int i = 0; i < 3; i++){
-            hsbInt[i] = (int)hsb[i] + threshold;
-        }
-        return hsbInt;
-    }
      
-    private boolean isBlue(Color color, float[] hsbvals) {
-        return hsbvals[0] <= hsbHighBlue[0] && hsbvals[0] >= hsbLowBlue[0] &&
-        hsbvals[1] <= hsbHighBlue[1] && hsbvals[1] >= hsbLowBlue[1] &&
-        hsbvals[2] <= hsbHighBlue[2] && hsbvals[2] >= hsbLowBlue[2] &&
-        color.getRed() <= thresholdsState.getBlue_r_high() && color.getRed() >= thresholdsState.getBlue_r_low() &&
+    private boolean isBlue(Color color) {
+        return color.getRed() <= thresholdsState.getBlue_r_high() && color.getRed() >= thresholdsState.getBlue_r_low() &&
         color.getGreen() <= thresholdsState.getBlue_g_high() && color.getGreen() >= thresholdsState.getBlue_g_low() &&
         color.getBlue() <= thresholdsState.getBlue_b_high() && color.getBlue() >= thresholdsState.getBlue_b_low();
     }
@@ -602,11 +567,8 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
      *                      thresholds (and thus the pixel is part of the yellow T),
      *                      false otherwise.
      */
-    private boolean isYellow(Color colour, float[] hsbvals) {
-        return hsbvals[0] <= hsbHighYellow[0] && hsbvals[0] >= hsbLowYellow[0] &&
-        hsbvals[1] <= hsbHighYellow[1] && hsbvals[1] >= hsbLowYellow[1] &&
-        hsbvals[2] <= hsbHighYellow[2] && hsbvals[2] >= hsbLowYellow[2] &&
-        colour.getRed() <= thresholdsState.getYellow_r_high() &&  colour.getRed() >= thresholdsState.getYellow_r_low() &&
+    private boolean isYellow(Color colour) {
+        return colour.getRed() <= thresholdsState.getYellow_r_high() &&  colour.getRed() >= thresholdsState.getYellow_r_low() &&
         colour.getGreen() <= thresholdsState.getYellow_g_high() && colour.getGreen() >= thresholdsState.getYellow_g_low() &&
         colour.getBlue() <= thresholdsState.getYellow_b_high() && colour.getBlue() >= thresholdsState.getYellow_b_low();
     }
@@ -622,11 +584,8 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
      *                      thresholds (and thus the pixel is part of the ball),
      *                      false otherwise.
      */
-    private boolean isBall(Color colour, float[] hsbvals) {
-        return hsbvals[0] <= hsbHighBall[0] && hsbvals[0] >= hsbLowBall[0] &&
-        hsbvals[1] <= hsbHighBall[1] && hsbvals[1] >= hsbLowBall[1] &&
-        hsbvals[2] <= hsbHighBall[2] && hsbvals[2] >= hsbLowBall[2] &&
-        colour.getRed() <= thresholdsState.getBall_r_high() &&  colour.getRed() >= thresholdsState.getBall_r_low() &&
+    private boolean isBall(Color colour) {
+        return colour.getRed() <= thresholdsState.getBall_r_high() &&  colour.getRed() >= thresholdsState.getBall_r_low() &&
         colour.getGreen() <= thresholdsState.getBall_g_high() && colour.getGreen() >= thresholdsState.getBall_g_low() &&
         colour.getBlue() <= thresholdsState.getBall_b_high() && colour.getBlue() >= thresholdsState.getBall_b_low();
     }
@@ -642,11 +601,8 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
      *                      thresholds (and thus the pixel is part of a grey circle),
      *                      false otherwise.
      */
-    private boolean isGrey(Color colour, float[] hsbvals) {
-        return hsbvals[0] <= hsbHighGrey[0] && hsbvals[0] >= hsbLowGrey[0] &&
-        hsbvals[1] <= hsbHighGrey[1] && hsbvals[1] >= hsbLowGrey[1] &&
-        hsbvals[2] <= hsbHighGrey[2] && hsbvals[2] >= hsbLowGrey[2] &&
-        colour.getRed() <= thresholdsState.getGrey_r_high() &&  colour.getRed() >= thresholdsState.getGrey_r_low() &&
+    private boolean isGrey(Color colour) {
+        return colour.getRed() <= thresholdsState.getGrey_r_high() &&  colour.getRed() >= thresholdsState.getGrey_r_low() &&
         colour.getGreen() <= thresholdsState.getGrey_g_high() && colour.getGreen() >= thresholdsState.getGrey_g_low() &&
         colour.getBlue() <= thresholdsState.getGrey_b_high() && colour.getBlue() >= thresholdsState.getGrey_b_low();
     }
@@ -662,11 +618,8 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
      *                      thresholds (and thus the pixel is part of a green plate),
      *                      false otherwise.
      */
-    private boolean isGreen(Color colour, float[] hsbvals) {
-        return hsbvals[0] <= hsbHighGreen[0] && hsbvals[0] >= hsbLowGreen[0] &&
-        hsbvals[1] <= hsbHighGreen[1] && hsbvals[1] >= hsbLowGreen[1] &&
-        hsbvals[2] <= hsbHighGreen[2] && hsbvals[2] >= hsbLowGreen[2] &&
-        colour.getRed() <= thresholdsState.getGreen_r_high() &&  colour.getRed() >= thresholdsState.getGreen_r_low() &&
+    private boolean isGreen(Color colour) {
+        return colour.getRed() <= thresholdsState.getGreen_r_high() &&  colour.getRed() >= thresholdsState.getGreen_r_low() &&
         colour.getGreen() <= thresholdsState.getGreen_g_high() && colour.getGreen() >= thresholdsState.getGreen_g_low() &&
         colour.getBlue() <= thresholdsState.getGreen_b_high() && colour.getBlue() >= thresholdsState.getGreen_b_low();
     }
@@ -762,9 +715,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
                 int greyY = meanY - (int) (ay * i);
                 try {
                     Color c = new Color(image.getRGB(greyX, greyY));
-                    float hsbvals[] = new float[3];
-                    Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
-                    if (isGrey(c, hsbvals)) {
+                    if (isGrey(c)) {
                         greyXPoints.add(greyX);
                         greyYPoints.add(greyY);
                     }
@@ -803,9 +754,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
             for (int y = (int) (backY-9); y < backY; y++) {
                 try {
                     Color c = new Color(image.getRGB(x, y));
-                    float hsbvals[] = new float[3];
-                    Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
-                    if (isGreen(c, hsbvals)) {
+                    if (isGreen(c)) {
                         foundGreen++;
                         break;
                     }
@@ -826,9 +775,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
             for (int y = (int) (backY); y < backY+10; y++) {
                 try {
                     Color c = new Color(image.getRGB(x, y));
-                    float hsbvals[] = new float[3];
-                    Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
-                    if (isGreen(c, hsbvals)) {
+                    if (isGreen(c)) {
                         foundGreen++;
                         break;
                     }
@@ -849,9 +796,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
             for (int y = (int) (backY-2); y < backY+3; y++) {
                 try {
                     Color c = new Color(image.getRGB(x, y));
-                    float hsbvals[] = new float[3];
-                    Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
-                    if (isGreen(c, hsbvals)) {
+                    if (isGreen(c)) {
                         foundGreen++;
                         break;
                     }
@@ -871,9 +816,7 @@ public class VisionFeed extends WindowAdapter implements MouseListener, MouseMot
             for (int y = (int) (backY-2); y < backY+3; y++) {
                 try {
                     Color c = new Color(image.getRGB(x, y));
-                    float hsbvals[] = new float[3];
-                    Color.RGBtoHSB(c.getRed(), c.getBlue(), c.getGreen(), hsbvals);
-                    if (isGreen(c, hsbvals)) {
+                    if (isGreen(c)) {
                         foundGreen++;
                         break;
                     }
