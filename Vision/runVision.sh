a384312a110e9ffff8bfdb23f51b2b7ca765594a
Tomas Tauber <tomas.tauber@gmail.com>
2012-01-28 20:05:10 +0000

    Vision refactor: deleted old structure - libs are now in the group folder, source code in src package structure

--- Vision/runVision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib:/group/teaching/sdp/sdp7/OpenCV/OpenCV-2.3.1/release/lib
-
-java -cp ./lib/v4l4j.jar:./lib/javacpp.jar:./lib/javacv.jar:./lib/javacv-linux-x86_64.jar:. TestVision
