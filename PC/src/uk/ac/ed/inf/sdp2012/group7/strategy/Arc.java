5da8ce1312b05181363fda7dd0f49976cefd3711
David Fraser <david.fraser04@gmail.com>
2012-02-07 16:36:06 +0000

    Intermediate work

---
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/Arc.java
@@ -0,0 +1,16 @@
+package uk.ac.ed.inf.sdp2012.group7.strategy;
+
+import java.awt.geom.*;
+
+/**
+ * Defines an arc to be used for a robot to drive on
+ * 
+ * @author David Fraser - s0912336
+ *
+ */
+public class Arc {
+	
+	private double radius;
+	
+
+}
---
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/ControlInterface.java
@@ -0,0 +1,31 @@
+package uk.ac.ed.inf.sdp2012.group7.strategy;
+
+/**
+ * This takes a small section of the plan from Control Management and turns it 
+ * into actual commands. 
+ * @author David Fraser -s0912336
+ *
+ */
+public class ControlInterface {
+	
+	
+	/**
+	 * Takes a small number of waypoints from Control Management 
+	 */
+	public void getNextMovement() {
+		
+	}
+	
+	public void getDistanceTravelled() {
+		
+	}
+	
+	public void chooseArc() {
+		
+	}
+	
+	public void implimentArc() {
+		
+	}
+
+}
