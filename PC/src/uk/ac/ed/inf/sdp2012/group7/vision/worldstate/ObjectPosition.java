c5982398076d79a9c038f5e04a9753a0e512e265
James Hulme <s0901522@sms.ed.ac.uk>
2012-02-12 14:26:47 +0000

    ui and gui gone through, now the real work begins

--- PC/src/uk/ac/ed/inf/sdp2012/group7/vision/worldstate/ObjectPosition.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/vision/worldstate/ObjectPosition.java
@@ -19,9 +19,6 @@ public class ObjectPosition{
     private Point bottomRight = new Point(0,0);
     private Point centre = new Point(0,0);
     
-    public ObjectPosition(){
-        
-    }
 
     public ObjectPosition(int x, int y){
         this.centre = new Point(x,y);
