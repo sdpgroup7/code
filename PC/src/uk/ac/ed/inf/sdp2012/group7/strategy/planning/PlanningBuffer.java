26abe0a88e30ab7513ec9b4c77b1b67b4334ffee
blasedef <blasedef@gmail.com>
2012-02-26 12:12:30 +0000

    added logger to buffer

--- PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/planning/PlanningBuffer.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/planning/PlanningBuffer.java
@@ -6,6 +6,8 @@ package uk.ac.ed.inf.sdp2012.group7.strategy.planning;
 import java.util.Observable;
 import java.util.Observer;
 
+import org.apache.log4j.Logger;
+
 /**
  * 
  */
@@ -17,6 +19,8 @@ import java.util.Observer;
 public class PlanningBuffer extends Observable implements Observer {
 
 	private Plan held_plan;
+	public static final Logger logger = Logger.getLogger(Plan.class);
+
 	
 	public PlanningBuffer(Observer myWatcher){
 		   
@@ -27,6 +31,7 @@ public class PlanningBuffer extends Observable implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		synchronized(this){
+			logger.debug("Planning Buffer Updated");
 			this.held_plan = (Plan)arg;;
 			setChanged();
 			notifyObservers(held_plan);
