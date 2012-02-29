1f31cb065175a0c779428962149233de258eb1d6
wiktor w brodlo <wiktor@brodlo.net>
2012-02-29 04:02:10 +0000

    generate basic world

--- PC/src/uk/ac/ed/inf/sdp2012/group7/vision/SimulatorFeed.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/vision/SimulatorFeed.java
@@ -15,6 +15,13 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import net.phys2d.math.Vector2f;
+import net.phys2d.raw.Body;
+import net.phys2d.raw.StaticBody;
+import net.phys2d.raw.World;
+import net.phys2d.raw.shapes.Box;
+import net.phys2d.raw.strategies.QuadSpaceStrategy;
+
 public class SimulatorFeed extends WindowAdapter {
 	private JLabel label;
 	private JFrame windowFrame;
@@ -31,6 +38,8 @@ public class SimulatorFeed extends WindowAdapter {
 	private Socket       socket;
 	private InputStream  is;
 	
+	private World world;
+	
 	/**
 	 * Default constructor.
 	 *
@@ -46,6 +55,7 @@ public class SimulatorFeed extends WindowAdapter {
 
 		/* Initialise the GUI that displays the video feed. */
 		initGUI();
+		initWorldSimulation();
 		initFrameGenerator();
 		Simulator.worldState.setClickingDone(true);
 	}
@@ -114,12 +124,58 @@ public class SimulatorFeed extends WindowAdapter {
 		
 	};
 	
+	/**
+	 * Starts the frame generator
+	 */
+	
 	private void initFrameGenerator() {
 		
 		receiver.run();
 		
 	}
 
+	/**
+	 * Creates the world
+	 */
+	private void initWorldSimulation() {
+		/* Initialise world */
+		world = new World(new Vector2f(0, 10), 10, new QuadSpaceStrategy(20, 5));
+		world.clear();
+		world.setGravity(0, 0);
+		
+		/* Build walls */
+		Body topWall = new StaticBody("top_wall", new Box(640, 104));
+		topWall.setPosition(0, 0);
+		topWall.setRestitution(1);
+		world.add(topWall);
+		
+		Body bottomWall = new StaticBody("bottom_wall", new Box(640, 104));
+		bottomWall.setPosition(0, 384);
+		bottomWall.setRestitution(1);
+		world.add(bottomWall);
+		
+		/* Needs to be done this way because we want to leave a hole for the goals */
+		Body leftWall_top = new StaticBody("left_top_wall", new Box(40, 196));
+		leftWall_top.setPosition(0, 0);
+		leftWall_top.setRestitution(1);
+		world.add(leftWall_top);
+		
+		Body leftWall_bottom = new StaticBody("left_bottom_wall", new Box(40, 196));
+		leftWall_bottom.setPosition(0, 337);
+		leftWall_bottom.setRestitution(1);
+		world.add(leftWall_bottom);
+		
+		Body rightWall_top = new StaticBody("right_top_wall", new Box(40, 196));
+		rightWall_top.setPosition(607, 0);
+		rightWall_top.setRestitution(1);
+		world.add(rightWall_top);
+		
+		Body rightWall_bottom = new StaticBody("right_bottom_wall", new Box(40, 196));
+		rightWall_bottom.setPosition(607, 337);
+		rightWall_bottom.setRestitution(1);
+		world.add(rightWall_bottom);
+	}
+	
 	/**
 	 * Creates the graphical interface components and initialises them
 	 */
