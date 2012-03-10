eac34c0fd86f38dd2f0f01bbafc3d4e0f0ff3648
blasedef <blasedef@gmail.com>
2012-03-10 15:45:19 +0000

    changed ASO to include center to kicker distance, changed TargetDecision so everything is in nodes

--- PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/planning/AllStaticObjects.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/planning/AllStaticObjects.java
@@ -38,6 +38,7 @@ public class AllStaticObjects {
 	private int height;
 	private int width;
 	private int boundary;
+	private int centreToEndOfKicker;
 	private double nodeInPixels;
 	private double widthOfPitchInRobotsCM;
 	private double widthOfRobotInNodes;
@@ -103,6 +104,8 @@ public class AllStaticObjects {
 		//with 58 as width, boundary is set at 3 nodes
 		this.boundary = (int)Math.ceil(this.widthOfRobotInNodes / 2);
 		
+		
+		this.centreToEndOfKicker = 6;
 		
 		//set defence position
 		this.pointInfrontOfGoal();
@@ -211,30 +214,30 @@ public class AllStaticObjects {
 		return this.boundary;
 	}
 	
-	public Point getTheirTopGoalPost() {
+	public Node getTheirTopGoalPost() {
 		return theirTopGoalPost;
 	}
 	
-	public Point getTheirBottomGoalPost() {
+	public Node getTheirBottomGoalPost() {
 		return theirBottomGoalPost;
 	}
 	
-	public Point getInFrontOfOurGoal() {
+	public Node getInFrontOfOurGoal() {
 		this.pointInfrontOfGoal();
 		return inFrontOfOurGoal;
 	}
 	
-	public Point getInFrontOfTheirGoal() {
+	public Node getInFrontOfTheirGoal() {
 		this.pointInfrontOfTheirGoal();
 		return inFrontOfTheirGoal;
 	}
 	
-	public Point getCentreOfTheirGoal() {
+	public Node getCentreOfTheirGoal() {
 		this.centreOfTheirGoal();
 		return centreOfTheirGoal;
 	}
 	
-	public Point getCentreOfOurGoal() {
+	public Node getCentreOfOurGoal() {
 		this.centreOfOurGoal();
 		return centreOfOurGoal;
 	}
@@ -287,6 +290,12 @@ public class AllStaticObjects {
 
 	public int getPitchRightBuffer() {
 		return pitchRightBuffer;
+	}
+
+
+	public double getCentreToEndOfKicker() {
+		// TODO Auto-generated method stub
+		return this.centreToEndOfKicker;
 	}
 
 }
