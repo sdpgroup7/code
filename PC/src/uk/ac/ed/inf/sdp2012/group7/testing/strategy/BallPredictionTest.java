0a531e954aa8116d2df0bf059beca28b944b45d6
darie <darie2808@yahoo.com>
2012-03-11 18:01:04 +0000

    tests for ballprediction

--- PC/src/uk/ac/ed/inf/sdp2012/group7/testing/strategy/BallPredictionTest.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/testing/strategy/BallPredictionTest.java
@@ -18,46 +18,145 @@ public class BallPredictionTest {
 	
 	@Test
 	public void ballPredictionCalculationTest1() {
-		Node ball = new Node (new Point (1,1));
+		Node ball = new Node (0,0);
 		double angle = 0;
 		double velocity = 0;
 		double time = 5;
-		int direction = 1;
-		
-		Point expected = new Point(0,0);
-		Point actual = TargetDecision.ballPredictionCalculation(ball, direction, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
-		Assert.assertEquals(expected, actual);
+	
 		System.out.println("Running  ball prediction test 1");
+		Point expected = new Point(0,0);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
 	}
 	
 	@Test
 	public void ballPredictionCalculationTest2() {
-		Node ball = new Node(new Point (200,200));
+		Node ball = new Node(0,0);
 		double angle = 0;
 		double velocity = 1;
 		double time = 5;
-		int direction = 1;
-		System.out.println(ball);
+
+		System.out.println("Running ball prediction test 2");
 		Point expected = new Point(5,0);
-		Point actual = TargetDecision.ballPredictionCalculation(ball, direction, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
 		System.out.println("Actual = (" + actual.x+","+actual.y+")");
 		Assert.assertEquals(expected, actual);
-		System.out.println("Running ball prediction test 2");
+		
 	}
 	@Test
 	public void ballPredictionCalculationTest3() {
-		Node ball = new Node(new Point (0,0));
-		double angle = Math.PI;
+		Node ball = new Node(0,0);
+		double angle = 0;
 		double velocity = 1;
-		double time = 5;
-		int direction = 1;
-		System.out.println(ball);
-		Point expected = new Point(53,0);
-		Point actual = TargetDecision.ballPredictionCalculation(ball, direction, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		double time = 3*58 +1;
+
+		System.out.println("Running ball prediction test 3");
+		Point expected = new Point(57,0);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
 		System.out.println("Actual = (" + actual.x+","+actual.y+")");
 		Assert.assertEquals(expected, actual);
-		System.out.println("Running ball prediction test 2");
+		
 	}
+	
+	@Test
+	public void ballPredictionCalculationTest4() {
+		Node ball = new Node(0,0);
+		double angle = Math.PI/2;
+		double velocity = 1;
+		double time = 4*29 +1;
+
+		System.out.println("Running ball prediction test 4");
+		Point expected = new Point(0,1);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void ballPredictionCalculationTest5() {
+		Node ball = new Node(0,0);
+		double angle = Math.PI;
+		double velocity = 1;
+		double time = 2*58+1;
+
+		System.out.println("Running ball prediction test 5");
+		Point expected = new Point(1,0);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	@Test
+	public void ballPredictionCalculationTest6() {
+		Node ball = new Node(0,0);
+		double angle = Math.PI*3/2;
+		double velocity = 1;
+		double time = 2*29+1;
+
+		System.out.println("Running ball prediction test 5");
+		Point expected = new Point(0,1);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	@Test
+	public void ballPredictionCalculationTest7() {
+		Node ball = new Node(0,1);
+		double angle = Math.PI;
+		double velocity = 2;
+		double time = 2*58+2;
+
+		System.out.println("Running ball prediction test 7");
+		Point expected = new Point(4,1);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	@Test
+	public void ballPredictionCalculationTest8() {
+		Node ball = new Node(1,1);
+		double angle = Math.PI/4;
+		double velocity = 1*Math.sqrt(2);
+		double time = 3;
+
+		System.out.println("Running ball prediction test 8");
+		Point expected = new Point(4,4);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void ballPredictionCalculationTest9() {
+		Node ball = new Node(0,0);
+		double angle = Math.PI/4 + Math.PI;
+		double velocity = 1*Math.sqrt(2);
+		double time = 3;
+
+		System.out.println("Running ball prediction test 8");
+		Point expected = new Point(3,3);
+		Point actual = TargetDecision.ballPredictionCalculation(ball, angle, velocity, time, pitchWidthinNodes, pitchHeightinNodes);
+		System.out.println("Expected = (" + expected.x+","+expected.y+")");
+		System.out.println("Actual = (" + actual.x+","+actual.y+")");
+		Assert.assertEquals(expected, actual);
+		
+	}
+	
+
 	
 	
 	
