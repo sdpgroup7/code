3d6ceaafd4a82f8def67367d68fb2560b55ec8ad
darie <darie2808@yahoo.com>
2012-03-15 17:20:21 +0000

    new plantype milestone4

--- PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/PlanTypes.java
+++ PC/src/uk/ac/ed/inf/sdp2012/group7/strategy/PlanTypes.java
@@ -3,10 +3,12 @@ package uk.ac.ed.inf.sdp2012.group7.strategy;
 public class PlanTypes {
 	
 	public static enum PlanType {
+		
 		PENALTY_DEFENCE,
 		PENALTY_OFFENCE,
 		FREE_PLAY,
 		HALT,
+		MILESTONE_4,
 	}
 	
 	public static enum ActionType {
