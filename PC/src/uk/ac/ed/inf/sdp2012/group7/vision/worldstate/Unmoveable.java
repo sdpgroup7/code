2bfdd504b38ec0a2a0e942114a9710afac33842c
David Fraser <david.fraser04@gmail.com>
2012-03-16 15:16:26 +0000

    got rid og shit I dont need

--- PC/src/uk/ac/ed/inf/sdp2012/group7/vision/worldstate/Unmoveable.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ed.inf.sdp2012.group7.vision.worldstate;
-
-import uk.ac.ed.inf.sdp2012.group7.vision.worldstate.ObjectPosition;
-
-public interface Unmoveable{
-
-    ObjectPosition getPosition();
-
-}
